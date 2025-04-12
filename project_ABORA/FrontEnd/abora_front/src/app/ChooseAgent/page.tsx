32af96b544c501bcb1d831ffc651725cc7a73db0
sanggyoon <sangreal99@naver.com>
2025-04-13 01:54:51 +0900

    style: 와이어 프레임 만드는 중

---
+++ project_ABORA/FrontEnd/abora_front/src/app/ChooseAgent/page.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from './page.module.css';
+
+export default function ChooseAgent() {
+  const router = useRouter();
+  const [currentSlideA, setCurrentSlideA] = useState(0);
+  const [currentSlideB, setCurrentSlideB] = useState(0);
+
+  // 선택된 아바타를 파라미터로 전달
+  const handleButtonClick = () => {
+    const agentA = slideData[currentSlideA];
+    const agentB = slideData[currentSlideB];
+    router.push(`/ConversationRoom?agentA=${agentA}&agentB=${agentB}`);
+  };
+
+  const slideData = [
+    'Avatar_GPT',
+    'Avatar_Gemini',
+    'Avatar_Claude',
+    'Avatar_Llama',
+  ];
+
+  const handleNextSlide = (
+    setSlide: React.Dispatch<React.SetStateAction<number>>,
+    currentSlide: number
+  ) => {
+    setSlide((currentSlide + 1) % slideData.length);
+  };
+
+  const handlePrevSlide = (
+    setSlide: React.Dispatch<React.SetStateAction<number>>,
+    currentSlide: number
+  ) => {
+    setSlide((currentSlide - 1 + slideData.length) % slideData.length);
+  };
+
+  return (
+    <div className={styles.chooseAgentContainer}>
+      {/* 슬라이드 A */}
+      <div className={styles.chooseAgent_A}>
+        <div
+          className={styles.arrow}
+          onClick={() => handlePrevSlide(setCurrentSlideA, currentSlideA)}
+        >
+          &#8592; {/* 왼쪽 화살표 */}
+        </div>
+        <div className={styles.slideBox}>
+          {slideData.map((item, index) => (
+            <div
+              key={index}
+              className={styles.slide}
+              style={{
+                transform: `translateX(${(index - currentSlideA) * 100}%)`,
+              }}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div
+          className={styles.arrow}
+          onClick={() => handleNextSlide(setCurrentSlideA, currentSlideA)}
+        >
+          &#8594; {/* 오른쪽 화살표 */}
+        </div>
+      </div>
+
+      {/* 슬라이드 B */}
+      <div className={styles.chooseAgent_B}>
+        <div
+          className={styles.arrow}
+          onClick={() => handlePrevSlide(setCurrentSlideB, currentSlideB)}
+        >
+          &#8592; {/* 왼쪽 화살표 */}
+        </div>
+        <div className={styles.slideBox}>
+          {slideData.map((item, index) => (
+            <div
+              key={index}
+              className={styles.slide}
+              style={{
+                transform: `translateX(${(index - currentSlideB) * 100}%)`,
+              }}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div
+          className={styles.arrow}
+          onClick={() => handleNextSlide(setCurrentSlideB, currentSlideB)}
+        >
+          &#8594; {/* 오른쪽 화살표 */}
+        </div>
+      </div>
+
+      <button
+        className={styles.moveConversationRoom}
+        onClick={handleButtonClick}
+      >
+        Select
+      </button>
+    </div>
+  );
+}
