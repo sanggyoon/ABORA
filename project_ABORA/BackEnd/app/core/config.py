b8292920c7ffe45d6982ea9424cf2e6a4c47f063
jungmin <1qsonjungmin@gmail.com>
2025-05-03 00:16:35 +0900

    feat: Add centralized configuration management with environment variables

---
+++ project_ABORA/BackEnd/app/core/config.py
@@ -0,0 +1,24 @@
+# app/core/config.py
+import os
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 65
+
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: str
+    db_name: str
+    server_name: str
+    database_url: str
+
+    model_config = ConfigDict(env_file=".env")
+
+settings = Settings()
