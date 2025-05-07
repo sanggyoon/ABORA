9838ac4bcfbb9ec4ebcf063c05eb8404d5d01730
jungmin <1qsonjungmin@gmail.com>
2025-05-07 14:37:43 +0900

    feat: Add routers for chatsession, and question modules

--- project_ABORA/BackEnd/app/main.py
+++ project_ABORA/BackEnd/app/main.py
@@ -1,9 +1,11 @@
+# app.main.py
 from fastapi import FastAPI
 from app.modules.user import router as user_router
 from app.modules.user.models import Base
 from app.db.session import engine
 from app.modules.auth.router import router as auth_router
-from app.modules.chatsession.router import router as chatsession_router  # chatsession 라우터 추가
+from app.modules.chatsession.router import router as chatsession_router
+from app.modules.question.router import router as question_router
 
 app = FastAPI()
 
@@ -13,4 +15,5 @@ Base.metadata.create_all(bind=engine)
 # 라우터 추가
 app.include_router(user_router)
 app.include_router(auth_router)
-app.include_router(chatsession_router)  # chatsession 라우터 등록
+app.include_router(chatsession_router)
+app.include_router(question_router)
