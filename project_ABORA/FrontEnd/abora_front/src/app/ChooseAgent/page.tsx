fe52e44a556d826ae368e5a225133144c3467921
chaeyoung <happy7yong83@gmail.com>
2025-05-07 22:38:12 +0900

    fix : 캐릭터 slideData glb 수정

--- project_ABORA/FrontEnd/abora_front/src/app/ChooseAgent/page.tsx
+++ project_ABORA/FrontEnd/abora_front/src/app/ChooseAgent/page.tsx
@@ -30,28 +30,28 @@ export default function ChooseAgent() {
       model: 'Chat-GPT',
       description: '사실적 기반 분석',
       Component: Avatar_GPT,
-      glb: '/models/chaeyoung-breath.glb',
+      glb: '/models/sanggyun.glb',
     },
     {
       name: '철학적인 동년',
       model: 'Claude',
       description: '철학적 기반 분석',
       Component: Avatar_Claude,
-      glb: '/models/chaeyoung-breath.glb',
+      glb: '/models/dongnyeon.glb',
     },
     {
       name: '감성적인 채영',
       model: 'Gemini',
       description: '감정 기반 분석',
       Component: Avatar_Gemini,
-      glb: '/models/chaeyoung-breath.glb',
+      glb: '/models/chaeyoung.glb',
     },
     {
       name: '실무적인 정민',
       model: 'Copilot',
       description: '실무적 기반 분석',
       Component: Avatar_Llama,
-      glb: '/models/chaeyoung-breath.glb',
+      glb: '/models/jungmin.glb',
     },
   ];
 
