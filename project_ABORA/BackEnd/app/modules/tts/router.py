074cba72299e53b8bf8c922d59c6c83bd05d91e0
chaeyoung <happy7yong83@gmail.com>
2025-05-19 11:48:02 +0900

    feat :  json get, delete 메소드

--- project_ABORA/BackEnd/app/modules/tts/router.py
+++ project_ABORA/BackEnd/app/modules/tts/router.py
@@ -4,6 +4,7 @@ import base64
 import requests
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
 from dotenv import load_dotenv
 import uuid
 from faster_whisper import WhisperModel
@@ -27,6 +28,7 @@ async def speak(request: Request):
     if not text:
         return { "error": "Text is empty." }
 
+    #tts 생성
     url = f"https://texttospeech.googleapis.com/v1/text:synthesize?key={GOOGLE_TTS_API_KEY}"
     payload = {
         "input": { "text": text },
@@ -41,6 +43,7 @@ async def speak(request: Request):
     audio_base64 = res.json()["audioContent"]
     audio_bytes = base64.b64decode(audio_base64)
 
+    #mp3 파일 경로
     filename = f"tts_{uuid.uuid4().hex}.mp3"
     filepath = f"public/tts/{filename}"
 
@@ -48,23 +51,50 @@ async def speak(request: Request):
     with open(filepath, "wb") as f:
         f.write(audio_bytes)
 
+    #mp3 파일을 json 파일로 변경 후 json 저장
     json_filename = filename.replace(".mp3", ".json")
     json_path = os.path.join("public", "json", json_filename)
 
+    #whisper 모듈 콜
     analyze_whisper(filepath, json_path)
 
 
     return JSONResponse({ "filename": filename, "json" : json_filename })
 
 
+#json
+@router.get("/json/{filename}")
+async def get_json(filename: str):
+    json_path = os.path.join("public", "json", filename)
+    if not os.path.exists(json_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    return FileResponse(json_path)
+
+
 #mp3 파일 삭제 라우터
 @router.delete("/{filename}")
 async def delete_mp3(filename: str):
-    filepath = f"public/tts/{filename}"
+    mp3_path = os.path.join("public", "tts", filename)
 
-    if os.path.exists(filepath):
-        os.remove(filepath)
-        return {"message": f"{filename} deleted"}
+    #mp3 파일을 json파일로 바꿔서 json 경로 생성
+    if filename.endswith(".mp3"):
+        json_filename = filename.replace(".mp3", ".json")
+        json_path = os.path.join("public", "json", json_filename)
+    else:
+        json_path = None
+
+
+    #mp3 삭제
+    if os.path.exists(mp3_path):
+        os.remove(mp3_path)
+
     else:
         raise HTTPException(status_code=404, detail="File not found")
+
+
+    # json 삭제 (존재할 경우만)
+    if json_path and os.path.exists(json_path):
+        os.remove(json_path)
+
+    return {"message": f"{filename} and its .json (if any) deleted"}
 
